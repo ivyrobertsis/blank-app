a20a8512deed7c54d466ccde3a1da54238d2eb2c
ivyrobertsis <irobert6@umd.edu>
2024-08-13 05:38:24 -0400

    testing with libraries

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,18 @@
+import streamlit as st
+
 import pandas as pd
 import seaborn as sns
-
-import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-sns.relplot(df)
+df = pd.read_csv("data.csv")
+df.rename(columns=lambda x: x[0].lower() + x.strip().lower().replace(' ', '_')[1:], inplace=True)
+df = df.convert_dtypes()
+df
+
+st.relplot(df)
 st.line_chart()
 st.write(df)
