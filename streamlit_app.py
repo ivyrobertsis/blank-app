9d55763933207c1845f95c8d1dba814307f86216
ivyrobertsis <irobert6@umd.edu>
2024-08-13 05:35:25 -0400

    testing basic plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+sns.relplot(df)
 st.line_chart()
 st.write(df)
